55ad40b989168fda4ceb9fe440873c31c2c64df4
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2024-02-23 18:54:37 +0100

    Server

---
+++ BanqueWs/src/main/java/ServerJWS.java
@@ -0,0 +1,11 @@
+import jakarta.xml.ws.Endpoint;
+import ws.BanqueService;
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        String url = "http://0.0.0.0:9191/";
+        Endpoint.publish(url, new BanqueService());
+        System.out.println("le serveur et deployer ");
+
+    }
+}
